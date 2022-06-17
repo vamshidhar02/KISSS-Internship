ad7a59f66a30ef48bb25dd18c5b1105b488aba89
unknown <vamshidhareddy11@gmail.com>
2022-06-17 11:37:20 +0530

    Tabular form

---
+++ script.js
@@ -0,0 +1,29 @@
+var entry = document.getElementById("entry");
+entry.addEventListener("click", displayDetails)
+ var row=1;
+ 
+ function displayDetails()
+ {
+     var name = document.getElementById("name").value;
+     var age = document.getElementById("age").value;
+     var email = document.getElementById("email").value;
+
+     if(!name || !age || !email)
+     {
+         alert("Please fill all the details");
+         return ;
+     }
+
+     var display = document.getElementById("display");
+    
+     var newRow = display.insertRow(row);
+     var cell1 = newRow.insertCell(0);
+     var cell2 = newRow.insertCell(1);
+     var cell3 = newRow.insertCell(2);
+
+     cell1.innerHTML = name;
+     cell2.innerHTML = age;
+     cell3.innerHTML = email;
+
+     row++;
+ }
